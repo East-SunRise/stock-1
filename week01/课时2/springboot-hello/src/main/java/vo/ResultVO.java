6108298318accde07d13eb5432e920598d7bfbc5
paze <2509159924@qq.com>
2026-03-08 18:07:59 +0800

    sub

---
+++ week01/课时2/springboot-hello/src/main/java/vo/ResultVO.java
@@ -0,0 +1,39 @@
+package vo;
+
+public class ResultVO<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    public ResultVO(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    // Getters and setters
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
+
